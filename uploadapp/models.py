3566011da706df1d13ef936c90d9e52adacf0321
JosephVC <josephvcardenas@gmail.com>
2020-04-29 17:35:49 -0700

    made changes to app models.py; other other additions  Please enter the commit message for your changes. Lines starting

--- uploadapp/models.py
+++ uploadapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from .models import File 
 
 
 # Create your models here.
---
+++ uploadapp/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  File 
+        fields = "__all__"
